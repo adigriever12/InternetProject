16a1d5d4082a23038c311fb1d0673ad3e8d548ff
adigriever <adigriever@walla.com>
2016-07-01 15:02:15 +0300

    some changes

--- client/admin/admin.controller.js
+++ client/admin/admin.controller.js
@@ -11,7 +11,7 @@
 
         vm.menu = [
             {name: 'Dashboard', link: '', img: 'glyphicon glyphicon-stats'},
-            {name: 'View Messages', link: 'message', img: 'glyphicon glyphicon-eye-open'},
+            {name: 'View Message', link: 'messages', img: 'glyphicon glyphicon-eye-open'},
             {name: 'New Messages', link: 'addEdit', img: 'glyphicon glyphicon-plus'},
             {name: 'Manage Data', link: '', img: 'glyphicon glyphicon-pencil'},
             {name: 'Display Live Screen', link: 'displayScreens', img: 'glyphicon glyphicon-blackboard'},
